2a67fe41a9ebf1cabe372bfd4f83608727d60932
Suman Kumar <skcool.123bgp@gmail.com>
2021-05-19 21:38:54 +0530

    Add linear progress

--- src/Components/Quiz/Timer/index.jsx
+++ src/Components/Quiz/Timer/index.jsx
@@ -3,17 +3,33 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import momentDurationFormatSetup from 'moment-duration-format';
 import Card from '@material-ui/core/Card';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { makeStyles } from '@material-ui/core/styles';
 import { TimeContainer } from './styled';
 
 momentDurationFormatSetup(moment);
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+});
 
 const Timer = ({ duration }) => {
+  const classes = useStyles();
+  const [progress, setProgress] = React.useState(0);
   const [time, setTime] = useState(duration);
+  let tracker = 0;
   useEffect(() => {
     const id = setInterval(() => {
-      clearInterval(id);
-      if (time !== 0) setTime(time - 1);
+      if (time !== 0) {
+        setTime(time - 1);
+        setProgress(progress + 100 / time);
+      }
     }, 1000);
+
+    return () => {
+      clearInterval(id);
+    };
   }, [time]);
 
   const displayTime = () => moment.duration(time, 'seconds').format('mm:ss');
@@ -21,6 +37,9 @@ const Timer = ({ duration }) => {
   return (
     <Card>
       <TimeContainer>{displayTime()}</TimeContainer>
+      <div className={classes.root}>
+        <LinearProgress variant="determinate" value={progress} />
+      </div>
     </Card>
   );
 };
